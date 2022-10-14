c46de6ea1509490a1de624cf011e84e9720c1014
Lelean Shaddod <lelean.shaddod@lit-co.net>
2022-10-14 21:55:42 +0300

    add models

---
+++ S22_AWP/Models/Function.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S22_AWP.Models
+{
+    public class Function
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string LongDescription { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
