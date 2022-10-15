4290bc69c9bb62e35e7af1a2fda5ee7012ed4a0c
Lelean Shaddod <lelean.shaddod@lit-co.net>
2022-10-15 17:13:11 +0300

    add Functions to homepage

--- S22_AWP/Controllers/HomeController.cs
+++ S22_AWP/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using S22_AWP.Data;
 using S22_AWP.Models;
+using S22_AWP.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,15 +15,20 @@ namespace S22_AWP.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                Functions = _context.Functions
+            };
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
@@ -32,6 +40,21 @@ namespace S22_AWP.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<IActionResult> FunctionDetailView(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var function = await _context.Functions.FirstOrDefaultAsync(m => m.ID == id);
+            if (function == null)
+            {
+                return NotFound();
+            }
+
+            return View(function);
         }
     }
 }
---
+++ S22_AWP/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using S22_AWP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S22_AWP.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Function> Functions;
+    }
+}
